36089d7f09d5636fb0cab81c370297bb22cdf053
sugureshwar.d <70892805+sugu-hub@users.noreply.github.com>
2021-05-12 12:32:43 +0530

    Update login.py

--- login.py
+++ login.py
@@ -5,9 +5,9 @@ import email
 import traceback
 
 
-ORG_EMAIL = "@gmail.com"
-FROM_EMAIL = "projectmini25" + ORG_EMAIL
-FROM_PWD = "miniproject25"
+ORG_EMAIL = "email name.com"
+FROM_EMAIL = "username" + ORG_EMAIL
+FROM_PWD = "password"
 SMTP_SERVER = "imap.gmail.com"
 SMTP_PORT = 993
 mail = imaplib.IMAP4_SSL(SMTP_SERVER)
